ff7399b47d39b3d9751d056314cf5da2dc01779c
gtortajada <gtortajada1@gmail.com>
2024-07-23 10:45:21 -0300

    Push Aula 14

---
+++ cursoEmVideo/aula 14/ambiente.js
@@ -0,0 +1,23 @@
+// var c = 1
+// while ( c <= 5) {
+//     console.log(c)
+//     c++
+// }
+
+// var c = 1
+// do {
+//     console.log(c)
+//     c++
+// } while (c <= 5)
+
+// A cima são duas formas diferentes de repetição (que terão o mesmo resultado), e agora será representado utilizando o for.
+
+// for (var c = 1; c <= 5; c++){   // Se não tivesse o "var" funcionaria, somente com "c = 1".
+//     console.log(c)
+// }
+
+console.log('Vai começar...')
+for(var c = 1; c <= 4 ; c++){
+    console.log(c)
+}
+console.log('Fim!')
