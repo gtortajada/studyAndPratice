a2796701ddba05ef010569a41a603312ebf3e6c3
gtortajada <gtortajada1@gmail.com>
2024-08-07 17:58:16 -0300

    aula 15

---
+++ cursoEmVideo/aula 15/ambiente.js
@@ -0,0 +1,24 @@
+// let num = [5,8,4,3]
+
+// num.push(7)
+// num.push(9)
+
+// console.log(`Nosso array é ${num}`)
+// console.log(`Nosso array tem ${num.length} elementos`)
+// console.log(`O primeiro elemento do nosso vetor antes do sort é ${num[0]}`)
+// console.log(`Nosso aray em ordem crescente é ${num.sort()}`)
+// console.log(`O primeiro elemento do nosso vetor depois do sort é ${num[0]}`)
+
+
+let valores = [5,2,1,4,7,22,21,25,53]
+
+// for ( let pos=0 ; pos < valores.length ; pos++ ){
+//     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+// }
+
+// valores.sort()
+// for (let pos in valores){
+//     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+// }
+
+console.log(valores.indexOf(7))
