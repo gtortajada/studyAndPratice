16f21d83410e30a695fef0b41903fae107124929
gtortajada <gtortajada1@gmail.com>
2024-08-26 18:38:13 -0300

    Correção de funcionalidade (não adicionar número repetido, mostrar o array no console, adição de mensagens de erro)

--- exercAnalisadorDeNumeros/script.js
+++ exercAnalisadorDeNumeros/script.js
@@ -1,16 +1,19 @@
-//O objetivo é adicionar o número tanto no quadro de baixo quanto em um array não mostrado no html.
+let arr = []
 
-let arr = [
-
-
-]
-
-function adicionarValor(num){
-    arr.push(num);
-    console.log(`Valor ${num} adicionado ao array.`)
+function adicionarValor(num) {
+    num = Number(num)
+    if (arr.includes(num)) {
+        window.alert(`O número ${num} já foi adicionado, insira outro número.`)
+        return
+    } else {
+        arr.push(num);
+        console.log(arr)
+        let log = window.document.getElementById('log');
+        log.innerHTML += `O número ${num} foi adicionado <br>`;
+    }
 }
 
-function mostrarAraay(){
+function mostrarArray() {
     console.log('Conteúdo do array', arr)
 }
 
@@ -20,35 +23,27 @@ function adiciona() {
     if (num.value.length == 0 || 1 > num.value || num.value > 100) {
         window.alert('[ERRO] Insira um número válido');
     } else {
-        let entrada = window.document.getElementById('entrada')
-        let result = window.document.getElementById('result')
-        let log = window.document.getElementById('log');
-        let add = window.document.getElementById('add')
-
-        adicionarValor()
-        mostrarAraay()
-        log.innerHTML += `O número ${num.value} foi adicionado<br>`;
-
+        adicionarValor(num.value)
+        mostrarArray()
         num.value = '';
-        num.focus();
-        //Adicionar em array / Adicionar no quadro a baixo
+        num.focus(); //Volta o cursor para adicionar o próximo número
     }
-
-
 }
 
-
-
-
-//Fazer o cáculo e mostrar: Ao todo são x números, O maior número é x, o menor número é x, A soma dos números é x e A média dos números é x.
-//Após adicionar vários números e ter o resultado (clicando em finalizar) se adicionar mais um número ele reseta as informações do array
 function finalizar() {
 
-    // if(){
+    // if(precisa ter número dentro do array, se não mensagem de erro){ 
 
+    // } else { se tiver número dentro do array vai chegar aqui e calcular tudo.
     // }
+    // Ao final precisa limpar o 'sistema' (array e <p> do log (número x adicionado)
+    //Fazer o cáculo e mostrar: Ao todo são x números, O maior número é x, o menor número é x, A soma dos números é x e A média dos números é x.
+    //Após adicionar vários números e ter o resultado (clicando em finalizar) se adicionar mais um número ele reseta as informações do array
 }
 
 
-// Se tentar adicionar sem colocar números precisa dar window.alert('[ERRO]').
 // Se tentar finalizar sem adicionar números, precisa dar window.alert('[ERRO]').
+
+//let add = window.document.getElementById('add')
+//let entrada = window.document.getElementById('entrada')
+//let result = window.document.getElementById('result')
