27c4aabd73c3a04fa5aba83c48818b87945681f8
gtortajada <gtortajada1@gmail.com>
2025-02-20 17:43:42 -0300

    feat: Criação de header + estilização

--- rocketseat/src/App.jsx
+++ rocketseat/src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post";
+import "./global.css";
+import { Header } from './components/Header'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Gustavo Tortajada"
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid hic aspernatur doloremque sit quisquam obcaecati rerum iste dolor placeat id ut provident, est ea, nulla nesciunt enim voluptatum illo delectus?"
